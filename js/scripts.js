310632b02133a0bef5e43155ee01bb8564cc13ab
Catherine Winters <catherine@catherinewinters.com>
2023-05-29 14:58:49 -0700

    added mixins, started editing theme, added QR code and leaflet sidebar libraries.

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,71 @@
 
   $(document).ready(function() {
     console.log("Questwalker theme ready.");
-    // initLeaflet();
-    getTourJSON();
+    addQRcode();
+    startTourNodes();
+    startTourEditor();
+
+
+    // delete this
+    var distance_between_london_and_paris = distance(51.5112139, -0.119824, 48.8567, 2.3508, 'K');
+    //round to 3 decimal places
+    console.log('London and Paris are ' + roundDistanceToKilometres(distance_between_london_and_paris) + ' kilometres and ' + roundDistanceToMetres(distance_between_london_and_paris) + ' metres apart.');
+    // console.log(Math.round(distance_between_london_and_paris*1000)/1000);  //displays 343.548
+    //end delete this
   });
 
+  // Run on the "view" version of tour nodes.
+  function startTourNodes() {
+    getTourJSON();
+  }
+
+  // Run on the "edit" version of tour nodes.
+  function startTourEditor() {
+    startTourNodeEditInlineForm();
+    updateInlineLocationField();
+  }
+
+
+  function updateInlineLocationField() {
+    // 1. Make sure the container is cleaned out.
+    $('#locations-editor ul').empty();
+    
+    // 2. Add the new locations.
+    $('form .field--name-field-locations .form-text').each(function() {
+      // var someText="Get the contents of the last brackets from: This is a (test) node (11)";
+      // console.log(someText.match(/\(([^)]*)\)[^(]*$/)[1]);
+      var node_id = $(this).val();
+      if (node_id != "") {
+        node_id = node_id.match(/\(([^)]*)\)[^(]*$/)[1];
+        // Returns the JSON for a location node at /node/12 with:
+        // https://questwalker.wintersandassociates.com/jsonapi/node/location/?filter[nid]=12
+        var json_to_import = '/jsonapi/node/location/?filter[nid]=' + node_id;
+        var json_response = $.getJSON(json_to_import, function() {
+        }).done(function() {
+          console.log(json_response.responseJSON);
+
+          // We assume startTourNodeEditInlineForm() has run, which means #locations-editor exists.
+          $('#locations-editor ul').append('<li>' + json_response.responseJSON.data[0].attributes.title + '</li>');
+        });
+      }
+    });
+  }
+
+  function startTourNodeEditInlineForm() {
+    if ($('#node-tour-edit-form .field--name-field-locations').length > 0) {
+
+      // 1. Add the container for the inline editor.
+      $('#node-tour-edit-form .field--name-field-locations').after('<div id="locations-editor"><h4 class="label">Locations editor</h4><ul></ul></div>')
+
+      // 2. Monitor "Locations on this tour" ordering on Tour nodes
+      const locations_on_this_tour_form_field = document.querySelector("#node-tour-edit-form .field--name-field-locations table");
+      const locations_on_this_tour_observer = new MutationObserver(() => {
+        console.log("The 'Locations on this Tour' field changed.");
+        updateInlineLocationField();
+      });
+      locations_on_this_tour_observer.observe(locations_on_this_tour_form_field, {subtree: true, childList: true, characterData: true});
+    }
+  }
 
   // For tours
   function getTourJSON() {
@@ -49,12 +110,12 @@
         var tilelayer_osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        });//.addTo(leaflet_map);
+        });
 
-        // var tilelayer_Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-        //   maxZoom: 20,
-        //   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        // }).addTo(leaflet_map);
+        var tilelayer_Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+          maxZoom: 20,
+          attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        });
 
         var tilelayer_stamen_toner = new L.StamenTileLayer("toner");
         // leaflet_map.addLayer(stamen_toner);
@@ -65,13 +126,13 @@
           fullscreenControlOptions: {
             position: 'topleft'
           },
-          layers: [tilelayer_osm, tilelayer_stamen_toner]
+          layers: [tilelayer_osm] // this only needs to be the default. (Can we get it from the node as a preset?)
         }).setView([51.505, -0.09], 13);
 
         var baseMaps = {
           "OpenStreetMap": tilelayer_osm,
           "Stamen Toner": tilelayer_stamen_toner,
-          // "Stadia OSM Bright": tilelayer_Stadia_OSMBright
+          "Stadia OSM Bright": tilelayer_Stadia_OSMBright
         };
         
         var overlayMaps = {
@@ -86,7 +147,8 @@
 
         function drawLocationFromJSON(location) {
           var location_title = location.attributes.title;
-    
+          var location_nid = location.attributes.drupal_internal__nid;
+
           // Coordinates:
           // location.field_coordinates
           // todo: iterate through this part.
@@ -103,11 +165,11 @@
             var location_long = location.attributes.field_coordinates.lon;
             console.log("Adding point marker at " + location_lat + ", " + location_long);
 
-            var marker = new L.marker([location_lat, location_long])
+            var marker = new L.marker([location_lat, location_long], {name: location_title, nid: location_nid})
               .addTo(leaflet_map)
               .bindPopup(location_title)
               .on('click', function(e) {
-                console.log(e.latlng);
+                handleClicksOnMarkers(e);
               });
             marker_locations.push([location_lat, location_long]);
           }
@@ -136,11 +198,11 @@
 
             
             // B. Actually add it to the map.
-            var polygon = L.polygon(polygon_data_array, { color: 'red'})
+            var polygon = L.polygon(polygon_data_array, {color: 'red', name: location_title, nid: location_nid})
               .addTo(leaflet_map)
               .bindPopup(location_title)
               .on('click', function(e) {
-                console.log(e.polygon_data_array);
+                handleClicksOnMarkers(e);
               });
 
             // 3. Iterate through all the coordinates of the polygon in order to get the marker bounds.
@@ -182,11 +244,11 @@
               var location_lat = this_point_only[1];
               var location_long = this_point_only[0];
               console.log(location_lat);
-              var marker = new L.marker([location_lat, location_long])
+              var marker = new L.marker([location_lat, location_long], {name: location_title, nid: location_nid})
                .addTo(leaflet_map)
                 .bindPopup(location_title)
                 .on('click', function(e) {
-                  // console.log(e.latlng);
+                  handleClicksOnMarkers(e);
                 });
               marker_locations.push([location_lat, location_long]);
             }
@@ -225,6 +287,22 @@
         }
         // END NEW
 
+
+        // When an object on the map is clicked, this fires.
+        function handleClicksOnMarkers(e) {
+          // console.log(e);
+          var nid = e.target.options.nid;
+
+          if ($('.view-locations-on-this-tour .node-' + nid).hasClass('active')) {
+            console.log("Closing location " + nid);
+            $('.view-locations-on-this-tour .views-row').removeClass('active'); // todo: do we also want "selected"?
+          }
+          else {
+            console.log("Opening details for this location: " + nid);
+            $('.view-locations-on-this-tour .views-row').removeClass('active'); // todo: do we also want "selected"?
+            $('.view-locations-on-this-tour .node-' + nid).addClass('active');
+          }
+        }
 
 
         // 4. Iterate through markers and add to map.
@@ -303,6 +381,10 @@
         });
         resizeObserver.observe(map);
 
+        addLeafletSidebar();
+        // Add the sidebar to the map.
+        var sidebar = L.control.sidebar('leaflet-sidebar').addTo(leaflet_map);
+
 
         // Add MarkerCluster
         // var markercluster = L.markerClusterGroup();
@@ -315,6 +397,117 @@
     // TODO: insert other cases where we would want to initialize Leaflet
     }
   }
+
+  function addLeafletSidebar() {
+
+    // <div id="sidebar" class="sidebar collapsed">
+    //   <!-- Nav tabs -->
+    //   <div class="sidebar-tabs">
+    //     <ul role="tablist">
+    //       <li><a href="#home" role="tab"><i class="fa fa-bars"></i></a></li>
+    //       <li><a href="#profile" role="tab"><i class="fa fa-user"></i></a></li>
+    //       <li class="disabled"><a href="#messages" role="tab"><i class="fa fa-envelope"></i></a></li>
+    //       <li><a href="https://github.com/Turbo87/sidebar-v2" role="tab" target="_blank"><i class="fa fa-github"></i></a></li>
+    //     </ul>
+
+    //     <ul role="tablist">
+    //       <li><a href="#settings" role="tab"><i class="fa fa-gear"></i></a></li>
+    //     </ul>
+    //   </div>
+
+    //   <!-- Tab panes -->
+    //   <div class="sidebar-content">
+    //     <div class="sidebar-pane" id="home">
+    //       <h1 class="sidebar-header">
+    //         sidebar-v2
+    //         <span class="sidebar-close"><i class="fa fa-caret-left"></i></span>
+    //       </h1>
+    //       <p>A responsive sidebar for mapping libraries like <a href="http://leafletjs.com/">Leaflet</a> or <a href="http://openlayers.org/">OpenLayers</a>.</p>
+
+    //       <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+    //       <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+    //       <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+    //       <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    //     </div>
+    //     <div class="sidebar-pane" id="profile">
+    //       <h1 class="sidebar-header">Profile<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+    //     </div>
+
+    //     <div class="sidebar-pane" id="messages">
+    //       <h1 class="sidebar-header">Messages<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+    //     </div>
+    //     <div class="sidebar-pane" id="settings">
+    //       <h1 class="sdebar-header">Settings<span class="sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+    //     </div>
+    //   </div>
+    // </div>
+    
+    var sidebar = L.control.sidebar('leaflet-sidebar').addTo(leaflet_map);
+
+  }
+
+
+  function tourNodeEditInlineForm() {
+    $('#edit-field-locations-wrapper').append('<div class="locations-inline-form"></div>');
+
+    // each 
+    // #lo
+    // <div class="drag-drawflow" draggable="true" ondragstart="drag(event)" data-node="template"><i class="fas fa-code"></i><span> Template</span></div>
+  }
+
+
+  
+  // add a QR code (Defaults to the current page.)
+  function addQRcode(url) {
+    if (url == undefined) {
+      url = window.location.href;
+    }
+    if ($('#qrcode').length == 0) {
+      $('footer').after('<div id="qrcode"></div>');
+      new QRCode(document.getElementById("qrcode"), url);
+    }
+  }
+
+  function distance(lat1, lon1, lat2, lon2, unit) {
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var radlon1 = Math.PI * lon1/180;
+    var radlon2 = Math.PI * lon2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit=="K") { dist = dist * 1.609344 }
+    if (unit=="N") { dist = dist * 0.8684 }
+    return dist
+  }
+
+  // 487.41 km turns into 487 km
+  // 5.41km turns into 5.4 km
+  // 0.551km turns into 551 m
+  function roundDistanceToKilometres(distance_in_km) {
+    return Math.round(distance_in_km);
+  }
+  function roundDistanceToMetres(distance_in_km) {
+    return Math.round(distance_in_km % 1 * 1000);
+  }
+
+  // Translation:
+  // this is to handle internationalization and localization correctly.
+  // 487.41 turns into [487, 410].
+  function getDistanceAsArray(distance_in_km) {
+    var km = roundDistanceToKilometres(distance_in_km);
+    var m = roundDistanceToMetres(distance_in_km);
+    return [km, m];
+  }
+
+  // return distance in miles and feet
+
 
   // set the view back to normal (exit AR mode, etc.)
   function resetView() {
@@ -336,4 +529,5 @@
       resetView();
     }
   });
+
 })(jQuery, Drupal, once);
